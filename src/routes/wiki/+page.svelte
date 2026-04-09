8e807bb60d65647523114ca7a003ff3800da0b39
Daveberry <84567826+daveberrys@users.noreply.github.com>
2026-04-09 17:13:49 +0800

    new website ig

---
+++ src/routes/wiki/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="js">
+    import Topbar from '../../webpack/topbar.svelte';
+    const page = 'wiki';
+</script>
+
+<main>
+    <Topbar page={page}/>
+    
+    <span>maybe later...</span>
+</main>
+
+<style>
+    span {
+        left: 50%;
+        top: 50%;
+        position: absolute;
+        transform: translate(-50%, -50%);
+    }
+</style>
