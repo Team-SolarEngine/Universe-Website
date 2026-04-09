9d3c28f8460cc86c507cbc0bb8967ac4af9d2fc1
Char <73309364+CharGoldenYT@users.noreply.github.com>
2026-04-09 12:10:13 -0400

    maybe last update before I get help???

--- src/routes/wiki/+page.svelte
+++ src/routes/wiki/+page.svelte
@@ -15,11 +15,11 @@ Unknown class MP4Handler |  Downgrade hxCodec to 2.5.1 <code>haxelib set hxCodec
 discord_rpc | "fatal error C1083: Cannot open include file: '../lib/discord-rpc/include/discord-rpc.h': No such file or directory" | do NOT directly install discord_rpc, run <code>haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc</code> <br>
 <br>
 <br>
-<b>Game Issues: <br>
-<b> Windows: </b>
+<b>Game Issues: </b><br>
+<b> Windows: </b><br>
 Certain lua based HUD elements are not colored correctly/at all - This will be fixed in a minor patch, if you downloaded 0.6.0's windows build, update to 0.6.1 when it releases.
 
-<b> Universal: </b>
+<b> Universal: </b><br>
 Result Screen does not properly exit song in some cases - Unknown Cause, cannot guarentee it will be able to be fixed.
 </span>
 </main>
