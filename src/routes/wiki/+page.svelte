652b29a755ffc110d244b58067c7f67d63ec51c3
Char <73309364+CharGoldenYT@users.noreply.github.com>
2026-04-09 21:36:05 -0400

    uh did I do that right?

--- src/routes/wiki/+page.svelte
+++ src/routes/wiki/+page.svelte
@@ -6,22 +6,29 @@
 <main>
     <Topbar page={page}/>
 
-    <span>The Wiki is not currently ready, take some commonly known bugs instead!<br>
-<br>
-<br>
-<b>Compiling Issues: </b><br>
-Unknown class MP4Handler |  Downgrade hxCodec to 2.5.1 <code>haxelib set hxCodec 2.5.1</code><br>
-<br>
-discord_rpc | "fatal error C1083: Cannot open include file: '../lib/discord-rpc/include/discord-rpc.h': No such file or directory" | do NOT directly install discord_rpc, run <code>haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc</code> <br>
-<br>
-<br>
-<b>Game Issues: </b><br>
-<b> Windows: </b><br>
-Certain lua based HUD elements are not colored correctly/at all - This will be fixed in a minor patch, if you downloaded 0.6.0's windows build, update to 0.6.1 when it releases.<br>
-<br>
-<b> Universal: </b><br>
-Result Screen does not properly exit song in some cases - Fixed in 0.6.1
-</span>
+    <span>
+        <div class="main">
+            <div class="mainContent background">
+                <section class="info">
+                    The Wiki is not currently ready, take some commonly known bugs instead!<br>
+                    <br>
+                    <br>
+                    <b>Compiling Issues: </b><br>
+                    Unknown class MP4Handler |  Downgrade hxCodec to 2.5.1 <code>haxelib set hxCodec 2.5.1</code><br>
+                    <br>
+                    discord_rpc | "fatal error C1083: Cannot open include file: '../lib/discord-rpc/include/discord-rpc.h': No such file or directory" | do NOT directly install discord_rpc, run <code>haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc</code> <br>
+                    <br>
+                    <br>
+                    <b>Game Issues: </b><br>
+                    <b> Windows: </b><br>
+                    Certain lua based HUD elements are not colored correctly/at all - This will be fixed in a minor patch, if you downloaded 0.6.0's windows build, update to 0.6.1 when it releases.<br>
+                    <br>
+                    <b> Universal: </b><br>
+                    Result Screen does not properly exit song in some cases - Fixed in 0.6.1
+                </section>
+            <div>
+        </div>
+    </span>
 </main>
 
 <style>
