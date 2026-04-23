00c74af5dffcaa9ab3b2050f75b1ac85c3720f52
Daveberry <84567826+daveberrys@users.noreply.github.com>
2026-04-23 22:08:04 +0800

    does this work

--- src/routes/api/news/+server.js
+++ src/routes/api/news/+server.js
@@ -4,14 +4,14 @@ import getNews from './all/getNews.js'
 
 export async function GET({ url }) {
   const action = url.searchParams.get('act');
-  const theURL = 'static/newsFiles'
-  
+  const theURL = 'newsFiles'
+
   if (action === 'getNews') {
     return await getNews(theURL);
   } else if (action === 'getNewsFile') {
     const fileName = url.searchParams.get('f');
     return await getNewsFile(fileName, theURL)
   }
-  
+
   throw error(400, 'Invalid action');
 }
